3c0cb4c8edfcfe4d49aa28ac04dba283217ece07
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-22 20:57:43 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,17 +3,17 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   def self.table_name
-    self.to_s.downcase.pluraliz
+    self.to_s.downcase.pluralize
   end
 
   def self.column_names
     DB[:conn].results_as_hash = true
-    sql = "pragma table_info('#{table_name}')'"
+    sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
-      column_names << row['name']
+      column_names << row["name"]
     end
     column_names.compact
   end
@@ -24,14 +24,13 @@ class InteractiveRecord
 
   def initialize(options={})
     options.each do |property, value|
-      self.send("#{property=}", value)
+      self.send("#{property}=", value)
     end
   end
 
   def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{column_names_for_insert} VALUES #{values_for_insert})"
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
-
     @id = DB[:conn].execute("select last_insert_rowid() from #{table_name_for_insert}")[0][0]
   end
 
@@ -39,7 +38,7 @@ class InteractiveRecord
     self.class.table_name
   end
 
-  def column_names_for_insert
+  def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
@@ -47,10 +46,19 @@ class InteractiveRecord
     values = []
     self.class.column_names.each do |column_name|
       values << "'#{send(column_name)}'" unless send(column_name).nil?
+    end
+    values.join(", ")
   end
 
   def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-  DB[:conn].execute(sql)
-end
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(attribute)
+    value = attribute.values.first
+    formatted = value.class == Fixnum ? value : "'#{value}'"
+    sql = "select * from #{self.table_name} where #{attribute.keys.first} = #{formatted}"
+    DB[:conn].execute(sql)
+  end
 end
