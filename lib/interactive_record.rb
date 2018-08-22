589b40475488871d6e49cec250ed50003d91136e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-22 20:36:36 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,47 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.table_name
+    self.to_s.downcase.pluraliz
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = "pragma table_info('#{table_name}')'"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row['name']
+    end
+    column_names.compact
+  end
+
+  column_names.each do |column_name|
+    attr_accessor column_name.to_sym
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property=}", value)
+    end
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{column_names_for_insert} VALUES #{values_for_insert})"
+    DB[:conn].execute(sql)
+
+    @id = DB[:conn].execute("select last_insert_rowid() from #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def column_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
+  end
 end
